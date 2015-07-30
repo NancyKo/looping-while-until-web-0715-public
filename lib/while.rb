8a0a26c498edfa592a1924e66d559125f6f06453
NancyKo <konancys@gmail.com>
2015-07-30 13:52:45 -0400

    Created basic while-until loops

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,12 @@
 def using_while
-	levitation_force = 6
-	
-	#your code here
+  #your code here
+
+  levitation_force = 6
+  phrase = "Wingardium Leviosa"
+  while levitation_force < 10
+    puts phrase 
+    levitation_force += 1
+  end
 end
 
 
